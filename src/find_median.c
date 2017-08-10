fc3b0bb1203c9d035d64f4de277a1615a09e8d2f
autopylot <wlin@e1z2r5p2.42.us.org>
2017-08-10 15:11:27 -0700

    Implement find_median

---
+++ src/find_median.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_median.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/10 12:35:08 by wlin              #+#    #+#             */
+/*   Updated: 2017/08/10 13:39:01 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "push_swap.h"
+#include <stdio.h>
+
+void swap_ele(int *a, int *b)
+{
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int	partition(int *arr, int low, int high)
+{
+	int pivot;
+	int i;
+	int j;
+
+	pivot = arr[high];
+	i = low - 1;
+	j = low;
+	while (j <= high - 1)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			swap_ele(&arr[i], &arr[j]);
+		}
+		++j;
+	}
+	swap_ele(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void qiksort(int *arr, int start, int end)
+{
+	int part;
+
+	if (start < end)
+	{
+		part = partition(arr, start, end);
+		qiksort(arr, start, part - 1);
+		qiksort(arr, part + 1, end);
+	}
+}
+
+int find_median(int *arr, int len)
+{
+	qiksort(arr, 0, len - 1);
+	return (arr[len / 2]);
+}
