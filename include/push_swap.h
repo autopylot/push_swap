db2c6dad06520f3805dbb2bdb4f4b528249ff881
autopylot <wleonlin@gmail.com>
2017-08-16 18:43:51 -0700

    Update header

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: wlin <wlin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 11:28:37 by wlin              #+#    #+#             */
-/*   Updated: 2017/08/02 13:47:11 by wlin             ###   ########.fr       */
+/*   Updated: 2017/08/16 11:39:00 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #define PUSH_SWAP_H
 
 #include "../libft/libft.h"
-
-
 
 typedef struct s_stack
 {
@@ -27,13 +25,36 @@ typedef struct s_stack
 void	init_stack(t_stack *s, int n);
 void	push(t_stack *s, int c);
 int		pop(t_stack *s);
-int		swap(t_stack *s);
-int		move(t_stack *a, t_stack *b);
-int		rotate(t_stack *s);
-int		rev_rotate(t_stack *s);
+int		swap(t_stack *s, char c);
+int		move(t_stack *a, t_stack *b, char c);
+int		rotate(t_stack *s, char c);
+int		rev_rotate(t_stack *s, char c);
 int		is_empty(t_stack *s);
+
+int		ft_error(char const *s, int fd);
+void 	print_stack(t_stack *s, char c);
 int		exe_instr(char *instr, t_stack *a, t_stack *b);
 int		read_instr(t_stack *a, t_stack *b);
-int		check_dup(t_stack *s, int n);
-int		valid_int(char *num);
+int		parse_nparams(t_stack *a, t_stack *b, int argc, char **argv);
+int		parse_nstring(t_stack *a, t_stack *b, char *str);
+
+void 	partial_sort(t_stack *s1, t_stack *s2);
+void	insort(t_stack *s1, t_stack *s2, char c);
+void	bubblesort(t_stack *s, char c);
+void	quicksort(t_stack *a, t_stack *b);
+
+void 	swap_top(t_stack *s, char c);
+int 	is_ordered(t_stack *s);
+int 	is_sorted(t_stack *s);
+void	roto_next(t_stack *s, int pos, char c);
+void	roto_both(t_stack *a, int aloc, t_stack *b, int bloc);
+
+int		find_optimal(t_stack *s1, t_stack *s2);
+int		find_ind(t_stack *s, int nbr);
+int		find_max(t_stack *s, int len, char c);
+int		find_min(t_stack *s, int size, char c);
+int		find_next(t_stack *s, int pivot, char c);
+int 	find_median(int *arr, int len);
+int		find_insert(int n, t_stack *s);
+
 #endif
